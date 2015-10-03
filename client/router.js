4bb03924eabb4de55104b524575e1b7f7db1504c
Maxime Quandalle <maxime@quandalle.com>
2015-10-03 15:33:13 +0200

    Support prefixed path
    You can test support using
    
      ROOT_URL="http://localhost:3000/flow/" meteor
    
    We also add the `arillo:flow-router-helpers` dependency which makes
    this simple example a little bit more complex but is required in order
    to correctly generate prefixed paths in the views.

--- client/router.js
+++ client/router.js
@@ -1,10 +1,12 @@
 FlowRouter.route('/', {
+  name: 'home',
   action: function() {
     BlazeLayout.render("mainLayout", {content: "blogHome"});
   }
 });
 
 FlowRouter.route('/:postId', {
+  name: 'post',
   action: function() {
     BlazeLayout.render("mainLayout", {content: "blogPost"});
   }
